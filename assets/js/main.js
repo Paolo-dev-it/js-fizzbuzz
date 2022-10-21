2b7c510c4743db1e309536856c28c418ecf2e717
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-10-21 15:05:39 +0200

    Created folders and files. Completed js code

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+
+for (let x = 1; x <= 100; x++){
+
+    if ( x % 3 == 0 && x % 5 == 0 ){
+        console.log(`${x} FIZZBUZZ`)
+    } else if ( x % 3 == 0 ){         
+        console.log(`${x} FIZZ`)
+    } else if ( x % 5 == 0 ){         
+        console.log(`${x} BUZZ`)
+    } else {         
+        console.log(`${x}`)
+    }
+}
+
