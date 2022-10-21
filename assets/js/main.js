ef2011942261d7793aab9ed4018786ae0d98f42e
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-10-21 17:19:14 +0200

    Working on bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,33 @@
 
+let container = document.getElementById("container");
 
-for (let x = 1; x <= 100; x++){
+let element = document.createElement("div");
 
-    if ( x % 3 == 0 && x % 5 == 0 ){
-        console.log(`${x} FIZZBUZZ`)
-    } else if ( x % 3 == 0 ){         
-        console.log(`${x} FIZZ`)
-    } else if ( x % 5 == 0 ){         
-        console.log(`${x} BUZZ`)
-    } else {         
-        console.log(`${x}`)
+element.classList.add('text-red',"fs-4", "p-5", "bg-success");
+
+container.classList.add("bg-red")
+
+container.append(element);
+
+
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 == 0 && i % 5 == 0){
+        // console.log(`${i} FIZZBUZZ`);
+        element.innerHTML += `<p class="bg-danger">${i} FIZZBUZZ</p>`;
+        
+        
+    }else if(i % 3 == 0){
+        // console.log(`${i} FIZZ`);
+        element.innerHTML += `<p class="bg-success">${i} FIZZ</p>`;
+
+    }else if (i % 5 == 0){
+        // console.log(`${i} BUZZ`);
+        element.innerHTML += `<p class="bg-warning">${i} BUZZ</p>`;
+
+    }else{
+        // console.log(`${i}`);
+        element.innerHTML += `<p class="bg-primary">${i}</p>`;
     }
 }
 
